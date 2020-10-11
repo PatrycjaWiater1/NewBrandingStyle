ddd4da02e8731d0d49f9cb17109f08f506918b2d
PatrycjaWiater1 <48619168+PatrycjaWiater1@users.noreply.github.com>
2020-10-11 17:58:48 +0200

    Add project files.

---
+++ NewBrandingStyle.Web/Controllers/ExchangesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewBrandingStyle.Web.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
