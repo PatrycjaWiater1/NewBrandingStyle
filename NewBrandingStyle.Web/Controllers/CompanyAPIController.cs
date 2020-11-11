365ade2107c8ea2251253720a23963a66dd89adc
PatrycjaWiater1 <48619168+PatrycjaWiater1@users.noreply.github.com>
2020-11-11 21:43:47 +0100

    JavaScript implemened, Index updated Fetch to finish

---
+++ NewBrandingStyle.Web/Controllers/CompanyAPIController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Models;
+
+namespace NewBrandingStyle.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyAPIController : ControllerBase
+    { 
+        [HttpPost]
+        [Route("AddNewItem")]
+        public IActionResult AddNewItem(CompanyModel companyModel)
+        {
+            return Ok("Item added succesfully!");
+        }
+    }
+}
---
+++ NewBrandingStyle.Web/wwwroot/js/addNewItem.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    console.log("ok")
+    const alertElement = document.getElementById("success-alert");
+    const formElement = document.forms[0];
+    formElement.addEventListener("submit", (event) => addnewItem(event))
+
+    const addnewItem = async () => {
+        const nameValue = document.getElementById("Name").value;
+        const descriptionValue = document.getElementById("Description").value;
+        const isVisibleValue = document.getElementById("IsVisible").checked;
+
+        const companyModel = {
+            Name: nameValue,
+            Description: descriptionValue,
+            IsVisible: isVisibleValue
+        };
+
+        const response = await fetch('/api/CompanyAPI/AddNewItem', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(companyModel)
+        })
+
+        const responseContent = await response.json();
+        debugger;
+    }
+})
