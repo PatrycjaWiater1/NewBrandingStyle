94f4e4d09cba1986855b67ede7203761eca0cee0
PatrycjaWiater1 <48619168+PatrycjaWiater1@users.noreply.github.com>
2020-11-26 22:09:57 +0100

    lab3 done

--- NewBrandingStyle.Web/Controllers/ExchangesController.cs
+++ NewBrandingStyle.Web/Controllers/ExchangesController.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NewBrandingStyle.Web.Database;
+using NewBrandingStyle.Web.Entities;
+using NewBrandingStyle.Web.Models;
 
 namespace NewBrandingStyle.Web.Controllers
 {
     public class ExchangesController : Controller
     {
-        public IActionResult Show()
+        private readonly ExchangesDbContext _dbContext;
+        public ExchangesController(ExchangesDbContext dbContext)
         {
-            return View();
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        [Route("AddNewItem")]
+        public IActionResult Add(CompanyModel companyModel)
+        {
+            var entity = new ItemEntity
+            {
+                Name = companyModel.Name,
+                Description = companyModel.Description,
+                IsVisible = companyModel.IsVisible,
+            };
+            _dbContext.Add(entity);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("GetItems")]
+        public async Task<IActionResult> GetItems()
+        {
+            var items = _dbContext.Items.ToList();
+
+            if (items == null)
+                return NotFound("Not Found");
+
+            return Ok(items);
         }
     }
 }
